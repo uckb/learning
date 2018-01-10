90060971355d57406f18a56566a46ca6fae7bb34
chenmeng <observerchan@gmail.com>
2018-01-10 23:21:07 +0800

    .

--- hello.c
+++ hello.c
@@ -1,15 +1,18 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/input.h>
+
 
 static int hello_init(void)
 {
-  printk(KERN_ALERT "Hello, world\n");
-  return 0;
+	printk(KERN_ALERT "Hello, world\n");
+	return 0;
 }
 
 static void hello_exit(void)
 {
-  printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, cruel world\n");
+	return 0;
 }
 
 
